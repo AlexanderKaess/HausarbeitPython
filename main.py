593b74daeb06548eae58d96fcca437a0ded85ea4
Alexander Kaess <kaessalexander@gmail.com>
2023-05-03 12:57:42 +0200

    add function get file content

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+def get_data_file_content(file_name):
+    with open(file_name, "r") as data_file:
+        data_file_content = data_file.read()
+
+    return data_file_content
+
+
 def main():
-    print("Hello World")
+    print("Starting script for record comparison and validation")
 
-
+    print(get_data_file_content("./Data/test.csv"))
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
