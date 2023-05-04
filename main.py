1ed3129e0b024b025f34d4b6a2340bfbffa575fd
Alexander Kaess <kaessalexander@gmail.com>
2023-05-04 09:21:58 +0200

    add logging to project

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import logging
+
+
 def get_data_file_content(file_name):
+    """
+    This function open its input-file in read-mode and return its content
+    :param file_name: the name of the file with path as string
+    :return: data_file_content: content of the input-file as string
+    """
     with open(file_name, "r") as data_file:
         data_file_content = data_file.read()
 
@@ -6,9 +14,12 @@ def get_data_file_content(file_name):
 
 
 def main():
-    print("Starting script for record comparison and validation")
+    logging.basicConfig(level=logging.INFO, filename="./Logging/info.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.info("Starting script for record comparison and validation")
 
     print(get_data_file_content("./Data/test.csv"))
+
+    logging.info("Finished script")
 
 
 if __name__ == "__main__":
