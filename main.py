54276005d71ad3bb0a6aa04aedaf0c89eb8f2d10
Alexander Kaess <kaessalexander@gmail.com>
2023-06-01 18:18:27 +0200

    create test calculation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import logging
-
 import pandas as pd
 import numpy as np
-import seaborn as sb
 
 import centrallogger
 import visualization
@@ -43,7 +41,37 @@ def main():
     ideal_visualization = visualization.Visualization(ideal_data, ideal_data.name)
     ideal_visualization.create_plot_from_dataframe()
 
+    test = least_square_calculation(train_data, ideal_data)
+    print(train_data)
+    print(test)
+
     print("### Finished script ###")
+
+
+def least_square_calculation(train_data, ideal_data):
+    result = 0.0
+    # get y values of each column from train and ideal
+
+    # create array from y column values
+    train_y_array = np.array(train_data.y1)
+    print(train_y_array)
+    ideal_y_array = np.array(ideal_data.y1)
+
+    # calculate least square deviation
+    result = np.square(np.subtract(train_y_array, ideal_y_array))
+
+    # numpy.org/doc/
+    # numpy.sqrt(x) = Quadratwurzel der Elemente von array x
+    # numpy.square(x) = Quadrat der Elemente von array x
+    # numpy.minimum(x1, x2) = Minimum der Elemente von array x1 und array x2
+    # numpy.std(x) = Berechnet Standardabweichung von array x
+    # a = np.array([1,2,3,4,5],[6,7,8,9,10])
+    # get a specific element a[1,3] --> 9
+    # get a specific row a[0,:] --> 1,2,3,4,5
+    # get a specific column a[:,2] --> 3,8
+
+
+    return result
 
 
 if __name__ == "__main__":
