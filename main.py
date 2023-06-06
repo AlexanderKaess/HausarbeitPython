d8486be9dd9833bd65ca6e0a53a93f490a11f851
Alexander Kaess <kaessalexander@gmail.com>
2023-06-06 18:45:28 +0200

    try to calculate to deviation in main.py

--- main.py
+++ main.py
@@ -36,28 +36,49 @@ def main():
     database.create_table_from_dataframe(test_data, test_data.name)
 
     # create plots
-    train_visualization = visualization.Visualization(train_data, train_data.name)
-    train_visualization.create_plot_from_dataframe()
-    ideal_visualization = visualization.Visualization(ideal_data, ideal_data.name)
-    ideal_visualization.create_plot_from_dataframe()
+    # train_visualization = visualization.Visualization(train_data, train_data.name)
+    # train_visualization.create_plot_from_dataframe()
+    # ideal_visualization = visualization.Visualization(ideal_data, ideal_data.name)
+    # ideal_visualization.create_plot_from_dataframe()
 
-    test = least_square_calculation(train_data, ideal_data)
-    print(test)
+    test_result = least_square_calculation(train_data, ideal_data)
+    print(test_result)
 
     print("### Finished script ###")
 
 
 def least_square_calculation(train_data, ideal_data):
     result = 0.0
-    # get y values of each column from train and ideal
 
-    # create array from y column values
-    train_y_array = np.array(train_data.y1)
-    print(train_y_array)
-    ideal_y_array = np.array(ideal_data.y1)
+    # get number of rows in train_data
+    rows_train_data = len(train_data.index)
+    print("rows = " + str(rows_train_data))
 
-    # calculate least square deviation
-    result = np.square(np.subtract(train_y_array, ideal_y_array))
+    # get number of columns in train_data
+    columns_train_data = len(train_data.columns)-1
+    print("columns_train_data = " + str(columns_train_data))
+
+    # get number of columns in ideal_data
+    # columns_ideal_data = len(ideal_data.columns)-1
+    columns_ideal_data = 10
+    print("columns_ideal_data = " + str(columns_ideal_data))
+
+    # 4 columns in train_data
+    for column_train in range(1, columns_train_data, 1):
+        print("train: " + str(column_train))
+        # 50 columns in ideal_data
+        for column_ideal in range(1, columns_ideal_data, 1):
+            print("ideal: " + str(column_ideal))
+            # create array from y column values
+            train_y_array = np.array(train_data["y" + str(column_train)])
+            ideal_y_array = np.array(ideal_data["y" + str(column_ideal)])
+
+            # 400 rows to subtract
+            for row in range(1, rows_train_data, 1):
+                # calculate least square deviation
+                print(str(row))
+                result = np.square(np.subtract(train_y_array[row], ideal_y_array[row]))
+                print(result)
 
     # numpy.org/doc/
     # numpy.sqrt(x) = Quadratwurzel der Elemente von array x
