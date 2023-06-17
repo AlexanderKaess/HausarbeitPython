bd1d138530d32a4c168f7755e7f0838b3eeb68c5
Alexander Kaess <kaessalexander@gmail.com>
2023-06-17 08:05:39 +0200

    refactor main.py and add class Calculation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import centrallogger
 import visualization
 from database import Database
+from calculation import Calculation
 
 logger = logging.getLogger("HAUSARBEIT")
 
@@ -41,67 +42,12 @@ def main():
     # ideal_visualization = visualization.Visualization(ideal_data, ideal_data.name)
     # ideal_visualization.create_plot_from_dataframe()
 
-    test_result = least_square_calculation(train_data, ideal_data)
+    calc = Calculation()
+    test_result = calc.least_square_calculation(train_data, ideal_data)
     print("testresult: " + str(test_result))
 
     print("### Finished script ###")
 
-
-def least_square_calculation(train_data, ideal_data):
-    result_dict_list = []
-
-    # get number of rows in train_data
-    rows_train_data = len(train_data.index)
-    print("rows = " + str(rows_train_data))
-
-    # get number of columns in train_data
-    columns_train_data = len(train_data.columns) - 1
-    print("columns_train_data = " + str(columns_train_data))
-
-    # get number of columns in ideal_data
-    columns_ideal_data = len(ideal_data.columns) - 1
-    # columns_ideal_data = 10
-    print("columns_ideal_data = " + str(columns_ideal_data))
-
-    # 4 columns in train_data
-    for column_train in range(1, columns_train_data + 1, 1):
-        sum_array = 0.0
-        result_dict = {"train_data_y": 0, "ideal_data_y": 0, "minimal_deviation_value": 0.0, "minimal_deviation_index": 0 }
-
-        result_dict["train_data_y"] = column_train
-        print("### train-function: y" + str(column_train) + " ###")
-        # 50 columns in ideal_data
-        new_sum_array = 100000.0
-        for column_ideal in range(1, columns_ideal_data + 1, 1):
-            current_column_sum = 0.0
-            # create array from y column values
-            train_y_array = np.array(train_data["y" + str(column_train)])
-            ideal_y_array = np.array(ideal_data["y" + str(column_ideal)])
-
-            # 400 rows to subtract
-            for row in range(0, rows_train_data, 1):
-                # calculate least square deviation
-                result = np.square(np.subtract(train_y_array[row], ideal_y_array[row]))
-                current_column_sum += result
-
-            sum_array = np.append(sum_array, current_column_sum)
-            index = 0
-            new_sum_array = np.delete(sum_array, index)
-
-        print("sum_array: " + str(new_sum_array))
-        minimal_deviation = np.min(new_sum_array)
-        result_dict["minimal_deviation_value"] = minimal_deviation
-        print("minimum deviation: " + str(minimal_deviation))
-
-        minimal_deviation_index = np.argmin(new_sum_array)
-        result_dict["minimal_deviation_index"] = minimal_deviation_index
-        result_dict["ideal_data_y"] = minimal_deviation_index + 1
-        print("minimum deviation index: " + str(minimal_deviation_index))
-        print("##### result_dict: " + str(result_dict))
-        print("")
-
-        result_dict_list.append(result_dict)
-    return result_dict_list
 
 
 if __name__ == "__main__":
